9be1dbae7eaf213f9e5fd00f99e9a7884aea716f
Sudhir <sudhir.rishi@gmail.com>
2021-04-17 20:01:08 +0530

    Changes - 17.04.2021

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -55,31 +55,48 @@ export class CheckoutComponent implements OnInit {
 		"language":"en_GB"
 	});
 	this.http.post(this.checkout_URL, data, { 'headers': headers }).subscribe((response)=>{
-				this.toastr.success("Check Out Successful", '');
-				//console.log(response);
-				localStorage.setItem('unique_id' , Object.values(response)[4]);
-				this.transactionStatus();
-			},
-			err => {
-				this.toastr.error("Transaction Failed", '');
-			}
-		)
+		localStorage.setItem('unique_id' , Object.values(response)[4]);
+		setTimeout(()=>{ this.transactionStatus()}, 1000);
+		},
+		err => {
+				this.toastr.error("Transaction Failed, Please Pair the Device Again", '');
+				this.router.navigate(['/']);
+		}
+	)
   }
   
   transactionStatus() {
-	this.unique_id = localStorage.setItem('unique_id');
+	this.unique_id = localStorage.getItem('unique_id');
 	this.sub_URL = '/transaction';
 	this.checkout_URL = this.base_URL+this.sub_URL+'?uti='+this.unique_id+'&tid='+this.serial_number;
 	
 	const headers= new HttpHeaders()
 		.set('Authorization', this.token)
 		.set('content-type', 'application/json')
-	
 	this.http.get(this.checkout_URL, { 'headers': headers }).subscribe((response)=>{
+		if(!Object.values(response)[33]) {
+			setTimeout(()=>{ this.transactionStatus()}, 2000);
+		} else {
 		console.log(response);
+		console.log(Object.values(response)[0]);
+		let statuses = JSON.parse(Object.values(response)[33]);
+		console.log(statuses);
+		if(statuses[4].statusDescription === 'Transaction Declined') {
+			this.toastr.error(statuses[4].statusDescription, '');
+			localStorage.removeItem('unique_id');
+			this.checkoutForm.reset();
+
+		} else {
+			this.toastr.success(statuses[4].statusDescription, '');
+			localStorage.removeItem('unique_id');
+			this.checkoutForm.reset();
+		}
+		}
+		
 	},
 	err => {
 		this.toastr.error("Error in Fetching Transaction Status", '');
 	}
 	)
+}
 }
